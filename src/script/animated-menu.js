6b76f5d279402ffaabf1add6f2fbb0f7f092266c
Mohamed Omar <Mohamed12@live.co.uk>
2020-01-08 18:51:07 +0000

    Animated Menu

---
+++ src/script/animated-menu.js
@@ -0,0 +1,8 @@
+function openNav () {
+  document.getElementById('mySidenav').style.height = 'inherit'
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav () {
+  document.getElementById('mySidenav').style.height = '0'
+}
