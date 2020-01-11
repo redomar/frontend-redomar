60291f746e15c5d699a15d072277a78e984481ee
Mohamed Omar <Mohamed12@live.co.uk>
2020-01-11 23:34:31 +0000

    Animated the navigation bar
    also removed remnants of node

--- src/script/animated-menu.js
+++ src/script/animated-menu.js
@@ -1,5 +1,13 @@
 function openNav () {
-  document.getElementById('mySidenav').style.height = 'inherit'
+  /* If the screen is small then stack nav vertically */
+  if (screen.width >= 450) {
+    document.getElementById('mySidenav').style.height = '50px'
+    document.getElementById('mySidenav').style.flexDirection = 'row'
+  } else {
+    document.getElementById('mySidenav').style.height = '300px'
+    document.getElementById('mySidenav').style.alignItems = 'center'
+    document.getElementById('mySidenav').style.flexDirection = 'column'
+  }
 }
 
 /* Set the width of the side navigation to 0 */
