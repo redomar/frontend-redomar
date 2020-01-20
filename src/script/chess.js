58f0f3d3b0e545c19582fe4af022b2acd21d225a
Mohamed Omar <Mohamed12@live.co.uk>
2020-01-20 23:58:56 +0000

    Added chess page with chartjs

---
+++ src/script/chess.js
@@ -0,0 +1,73 @@
+/**
+ * @author Mohamed Omar
+ * 20/Jan/2020
+ */
+
+//  Send fetch request
+async function sendApiRequest (url) {
+  const sendFetch = await fetch(url)
+  const response = await sendFetch.json()
+  return response
+}
+
+//  Fetch stats from the Chess.com api
+async function getChessStats () {
+  const stats = await sendApiRequest(
+    'https://api.chess.com/pub/player/royalered/stats'
+  )
+  document.title += ` Stats`
+  return stats
+}
+
+/*  Immediately invoked function that waits for response from api
+ *  after respose it will use chart.js to draw a doughnut chart
+ */
+;(async () => {
+  stats = await getChessStats()
+  score = stats.chess_blitz.last
+  scoreRecord = stats.chess_blitz.record
+  scoreRecord = [scoreRecord.loss, scoreRecord.draw, scoreRecord.win]
+
+  chartStats(...scoreRecord)
+  scoredDate = new Date(score.date * 1000).toLocaleString('en-GB', {
+    timeZone: 'Europe/London',
+    dateStyle: 'full',
+    timeStyle: 'short'
+  })
+  let selection = document.getElementById('chess')
+  let newArticle = document.createElement('article')
+  selection.appendChild(
+    newArticle
+  ).innerHTML = `<span>Currently ranked: <b>${score.rating}</b> as of ${scoredDate}</span>`
+})()
+
+//  Function that renders chart.js
+function chartStats (loss, draw, win) {
+  var ctx = document.getElementById('scoreChart').getContext('2d')
+  var scoreChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: [`Loss: ${loss}`, `Draw: ${draw}`, `Win: ${win}`],
+      datasets: [
+        {
+          data: [loss, draw, win],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.8)',
+            'rgba(192, 192, 192, 0.8)',
+            'rgba(75, 192, 192, 0.8)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(192, 192, 192, 1)',
+            'rgba(75, 192, 192, 1)'
+          ]
+        }
+      ]
+    },
+    options: {
+      legend: {
+        reverse: true
+      }
+    }
+  })
+}
