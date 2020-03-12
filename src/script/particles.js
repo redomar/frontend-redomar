76be8141ca67dd79e71ddced68da8558bbc181cd
Mohamed Omar <Mohamed12@live.co.uk>
2020-03-12 09:53:29 +0000

    Tweak design

--- src/script/particles.js
+++ src/script/particles.js
@@ -3,5 +3,7 @@ if (screen.width >= 450) {
   particlesJS.load('particles-js', 'assets/particles.json', function () {
     console.log('callback - particles.js config loaded')
     document.getElementById('particles-js').style.visibility = 'visible'
+    document.getElementById('location').style.filter = 'invert()'
+    document.getElementById('full-name').style.filter = 'invert()'
   })
 }
